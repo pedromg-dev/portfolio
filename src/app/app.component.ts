772861e04638d4627812e8664ee4998b2e2cbb19
Pedro Antonio Moya García <pedroantonino123@gmail.com>
2024-01-23 22:24:53 +0100

    UI changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslateService } from "@ngx-translate/core";
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'portfolio';
