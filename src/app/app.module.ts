11c1e60a356dcc2a203863838efb4aac63e1219b
Pedro Antonio Moya García <pedroantonino123@gmail.com>
2024-03-03 23:01:16 +0100

    ui changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,13 +29,13 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ProjectCardComponent } from './components/project-card/project-card.component';
 
 //Use this route in hosting (Production)
-// export function HttpLoaderFactory(http: HttpClient) {
-//   return new TranslateHttpLoader(http, "/portfolio/assets/i18n/", ".json");
-// }
-
-export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http);
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, "/portfolio/assets/i18n/", ".json");
 }
+
+// export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+//   return new TranslateHttpLoader(http);
+// }
 
 @NgModule({
   declarations: [
